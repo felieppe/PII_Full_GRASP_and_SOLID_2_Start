dafcfb83f1a217e90beffbd400a8bf0846758f92
felieppe <me@felieppe.com>
2023-09-06 12:55:19 -0300

    ConsolePrinter class created

---
+++ src/Library/ConsolePrinter.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Full_GRASP_And_SOLID.Library;
+
+namespace Library
+{
+    public class ConsolePrinter
+    {
+        public static void PrintRecipe(Recipe recipe)
+        {
+            Console.WriteLine($"Receta de {recipe.FinalProduct.Description}:");
+            foreach (Step step in recipe.GetSteps())
+            {
+                Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
+                    $"usando '{step.Equipment.Description}' durante {step.Time}");
+            }
+        }   
+    }
+}
